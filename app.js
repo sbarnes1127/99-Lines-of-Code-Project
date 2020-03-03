50757dee2d7ef9bc07e04c02dcf92a446b45888b
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-03-03 13:08:57 -0500

    finished part 1 of 99 lines of code lab

---
+++ app.js
@@ -0,0 +1,15 @@
+let Friends = ['John', 'Shelby', 'Rachel', 'Brenna', 'Liz'];
+
+for (f = 0; f < Friends.length; f++){
+    let friend = Friends[f]; {
+        for (s = 99; s > 0; s--) {
+            let minusOne = s - 1;
+            if (s > 1) {
+                console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
+            } else {
+                console.log("1 line of code in the file, 1 line of code, " + friend + " strikes one out, clears it all out, no more lines of code in the file");
+            }
+            
+        }
+    }
+}
