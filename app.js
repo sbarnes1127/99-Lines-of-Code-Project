d24dee34b3b6041219ab7405bdf5ada11fd23293
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-03-14 12:17:01 -0400

    made some changes after the code review, used let on s and f so they arent attached to the global scope of the document, console.group on the friends, switched the if statement around so the if is the niche scenario, and the else is the general scenario, and added an else if to fix the grammar on the 2nd line of code part

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let Friends = ['John', 'Shelby', 'Rachel', 'Brenna', 'Liz'];
 
-for (f = 0; f < Friends.length; f++){
-    let friend = Friends[f]; {
-        for (s = 99; s > 0; s--) {
-            let minusOne = s - 1;
-            if (s > 1) {
-                console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
-            } else {
-                console.log("1 line of code in the file, 1 line of code, " + friend + " strikes one out, clears it all out, no more lines of code in the file");
-            }
-            
+for (let f = 0; f < Friends.length; f++) {
+    let friend = Friends[f];
+    console.group(friend.toUpperCase());
+    for (let s = 99; s > 0; s--) {
+        let minusOne = s - 1;
+        if (s === 1) {
+            console.log("1 line of code in the file, 1 line of code, " + friend + " strikes one out, clears it all out, no more lines of code in the file");
+        } else if(s ===2) {
+            console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, 1 line of code in the file");
+        
+        } else {
+            console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
         }
+
     }
+    console.groupEnd();
 }
